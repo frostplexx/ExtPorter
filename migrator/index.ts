c3d5afb2e21a6236d0e86f3f9b7c88919909fd5e
daniel <daniel.inama02@gmail.com>
2025-11-07 11:34:16 +0100

    chore: refactor bridge_injector

--- migrator/index.ts
+++ migrator/index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import * as path from 'path';
-import { RenameAPIS } from './modules/api_renames/api_renames';
+import { RenameAPIS } from './modules/api_renames';
 import { MigrateManifest } from './modules/manifest';
 import { MigrateCSP } from './modules/csp';
 import { WriteMigrated } from './modules/write_migrated';
